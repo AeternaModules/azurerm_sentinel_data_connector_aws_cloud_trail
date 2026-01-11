4203c4d783865c3d297724379c7d4db2ee2c40b0
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:04:28 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_aws_cloud_trails" {
+  description = "All sentinel_data_connector_aws_cloud_trail resources"
+  value       = azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails
+}
+
