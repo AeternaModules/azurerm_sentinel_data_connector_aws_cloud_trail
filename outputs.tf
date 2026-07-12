283a2449563468b06d593fa815dbe8ad8e86161c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_aws_cloud_trails" {
-  description = "All sentinel_data_connector_aws_cloud_trail resources"
-  value       = azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails
-}
 output "sentinel_data_connector_aws_cloud_trails_aws_role_arn" {
-  description = "List of aws_role_arn values across all sentinel_data_connector_aws_cloud_trails"
-  value       = [for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : v.aws_role_arn]
+  description = "Map of aws_role_arn values across all sentinel_data_connector_aws_cloud_trails, keyed the same as var.sentinel_data_connector_aws_cloud_trails"
+  value       = { for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : k => v.aws_role_arn }
 }
 output "sentinel_data_connector_aws_cloud_trails_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_aws_cloud_trails"
-  value       = [for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_aws_cloud_trails, keyed the same as var.sentinel_data_connector_aws_cloud_trails"
+  value       = { for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_aws_cloud_trails_name" {
-  description = "List of name values across all sentinel_data_connector_aws_cloud_trails"
-  value       = [for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : v.name]
+  description = "Map of name values across all sentinel_data_connector_aws_cloud_trails, keyed the same as var.sentinel_data_connector_aws_cloud_trails"
+  value       = { for k, v in azurerm_sentinel_data_connector_aws_cloud_trail.sentinel_data_connector_aws_cloud_trails : k => v.name }
 }
 
